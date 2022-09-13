2f2ed9ea6aeb5a178519df21afd827dce2fe6404
Сонькин Алексей <yateplofizik@yandex.ru>
2022-09-13 14:24:47 +0300

    Lzma compression!

--- NyaFs/Filesystem/SquashFs/Compression/Lzma.cs
+++ NyaFs/Filesystem/SquashFs/Compression/Lzma.cs
@@ -13,7 +13,24 @@ namespace NyaFs.Filesystem.SquashFs.Compression
 
         internal override byte[] Compress(byte[] Data)
         {
-            throw new NotImplementedException();
+            SevenZip.Compression.LZMA.Encoder encoder = new SevenZip.Compression.LZMA.Encoder();
+
+            long outSize = Data.Length;
+            long inSize = Data.Length - 13;
+
+            using (var input = new MemoryStream(Data))
+            {
+                using (var output = new MemoryStream())
+                {
+                    byte[] Size = new byte[8];
+                    Size.WriteUInt64(0, Convert.ToUInt64(Data.Length));
+
+                    encoder.WriteCoderProperties(output);
+                    output.Write(Size);
+                    encoder.Code(input, output, input.Length, outSize, null);
+                    return output.ToArray();
+                }
+            }
         }
 
         internal override byte[] Decompress(byte[] Data)
